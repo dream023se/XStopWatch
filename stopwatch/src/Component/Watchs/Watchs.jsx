4ea1008aa345522b7a20517bb4772ed45a1083c6
dream023se <dubeysanchika82@gmail.com>
2024-05-07 17:26:19 +0530

    stopwatch

---
+++ stopwatch/src/Component/Watchs/Watchs.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+
+function Stopwatch() {
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let timer
+    if (isRunning) {
+      timer = setInterval(() => {
+        setTime(prevTime => prevTime + 1);
+      }, 1000);
+    } else {
+      clearInterval(timer);
+    }
+    return () => clearInterval(timer);
+  }, [isRunning]);
+
+  const startStopwatch = () => {
+    setIsRunning(prevState => !prevState);
+  };
+
+  const resetStopwatch = () => {
+    setTime(0);
+    setIsRunning(false);
+  };
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  };
+
+  return (
+    <div>
+      <h1>Stopwatch</h1>
+      <h2>Time: {formatTime(time)}</h2>
+      <div>
+        {isRunning ? (
+          <button onClick={startStopwatch}>Stop</button>
+        ) : (
+          <button onClick={startStopwatch}>Start</button>
+        )}
+        <button onClick={resetStopwatch}>Reset</button>
+      </div>
+    </div>
+  );
+}
+
+export default Stopwatch;
